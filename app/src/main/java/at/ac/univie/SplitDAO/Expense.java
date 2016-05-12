1ff612801ef5850c1888c17efe55fa8e8e4f91fc
Weinbacher  Andreas Lukas <a1407626@unet.univie.ac.at>
2016-05-12 18:20:38 +0200

    Trying to implement qr code generator

---
+++ app/src/main/java/at/ac/univie/SplitDAO/Expense.java
@@ -0,0 +1,64 @@
+package at.ac.univie.SplitDAO;
+
+import java.util.GregorianCalendar;
+import java.util.List;
+
+/**
+ * Created by Andy on 12.05.16.
+ */
+class Expense {
+    GregorianCalendar curr_date;
+    Friend creator;
+    double amount;
+    String description;
+    List<Notes> notes;  //notizen mit änderungen
+
+    //beide Listen gehören zusammen
+    List<Friend> participants;
+    List<double> spending;
+
+    enum Category { travelling, food, drinks, fun, moneytransfer, medical , misc } //maybe more
+    Category category;
+    Friend payer;
+    boolean complete = false;
+
+    Spliteven even;
+    Splitmanual manual;
+    Splitratio ratio;
+    Splitpart parts;
+
+
+
+    Expense (Friend creator, Friend payer, double amount, String description, Category category) {
+        this.creator = setcreator(creator);
+        this.payer = setpayer(payer);
+        this.amount = setamount(amount);
+        this.description = setdescription(description);
+        this.category = setcategory(category);
+
+    }
+
+    public boolean modifyparticipants(List<Friend> participants) {
+        this.participants
+        //pseudocode
+    	/*
+    	wenn participants nicht gesetzt, dann überschreiben
+    	Wenn neue participants gleiche anzahl haben, dann überschreiben
+    	wenn anzahl unterschiedlich, spending array zwischenspeichern. beträge die nicht auf neues participants
+    	array passen, auf payer aufsummieren
+
+		danach immer update der spendings. Spendingsgröße muss angepasst werden.
+    	*/
+
+        //update();
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/at/ac/univie/SplitDAO/Group.java
@@ -0,0 +1,14 @@
+package at.ac.univie.SplitDAO;
+
+import java.util.List;
+
+/**
+ * Created by Andy on 12.05.16.
+ */
+public class Group {
+    String groupname;
+    List<Friend> members;
+    List<Expense> expenses;
+
+
+}
